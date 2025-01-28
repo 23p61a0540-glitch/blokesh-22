cb0a27b3aa8cc159f758348a9ddb04fbc16fa17b
Tanishq Verma <mastertanishq193@gmail.com>
2025-01-28 12:36:47 +0530

    Initial commit with clean history

---
+++ Pro1/Week2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int a, b, c;
+	printf("Enter the FIRST number:\n");
+	scanf("%d",&a);
+	printf("Enter the SECOND number:\n");
+	scanf("%d",&b);
+	c = a + b;
+	printf("Sum of the two numbers is %d.", c);
+	return 0;
+}
